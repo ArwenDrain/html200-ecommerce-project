7912c89ddf08307cc88cdbfade8412820ff02afe
ArwenDrain <arwen.drain@gmail.com>
2018-03-08 00:15:59 -0800

    modified index.html, form-handler.js and main.css

--- js/form-handler.js
+++ js/form-handler.js
@@ -7,3 +7,16 @@ function capture(){
   console.log("Thanks for signing up for our mailing list, " + email + "!")
   alert("Thanks for signing up for our mailing list, " + email + "!")
 }
+
+//Add/remove from shopping cart
+var cart=[]
+function addToCart(productName){
+  event.preventDefault()
+  var i = cart.indexOf(productName)
+  if (i === -1){
+    cart.push(productName)
+  } else {
+    cart.splice(i,1)
+  }
+  console.log(cart.length + " item(s) added to your shopping cart.")
+}
